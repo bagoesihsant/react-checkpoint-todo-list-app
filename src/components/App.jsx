f201dbcce61e0a3b043d142dbf9a34a4fea2935e
bagoesihsant <bagoesihsant@gmail.com>
2025-09-29 11:47:59 +0700

    Changed layout from previous todo list, to a much simpler and elegant ones, also adding new icons for use.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,76 @@
+
+// Style and Images Import
 import '../styles/App.css'
+import plusIcon from '../assets/plus-icon.svg'
+import penIcon from '../assets/pen-icon.svg'
+import trashIcon from '../assets/trash-icon.svg'
 
 export default function App() {
 
     return (
-        <div className="container">
-            <div className="todo-app">
-                <div className="todo-header">
-                    <nav className="header-navigator">
-                        <button>
-                            Messages
-                        </button>
-                        <button className='active'>
-                            Today's Task
-                        </button>
-                        <button>
-                            Last Activity
-                        </button>
-                    </nav>
-                    <div className="add-task">
-                        <div className="dates">
-                            <h2>Today's Task</h2>
-                            <p>Wednesday, 11 May</p>
-                        </div>
-                        <div className="add">
-                            <button>New Task</button>
-                        </div>
-                    </div>
-                    <div className="remaining-tasks">
-                        <p className="all active">
-                            All <span>35</span>
-                        </p>
-                        <p className="open">
-                            Open <span>14</span>
-                        </p>
-                        <p className="closed">
-                            Closed <span>19</span>
-                        </p>
-                        <p className="archived">
-                            Archived <span>2</span>
-                        </p>
-                    </div>
-                </div>
-                <div className="todo-body">
-                    <div className="task-card">
-                        <div className="card-body">
-                            <div className="task">
-                                <h1>Client Review & Feedback</h1>
-                                <p>Crypto Wallet Redesign</p>
-                            </div>
-                            <div className="round">
-                                <input type="checkbox" name="task-check" id="task-check" />
-                                <label htmlFor="task-check"></label>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <span className="date">Today</span>
-                            <span className="time">10:00 PM - 11:45 PM</span>
-                        </div>
-                    </div>
-                    <div className="task-card">
-                        <div className="card-body">
-                            <div className="task">
-                                <h1>Client Review & Feedback</h1>
-                                <p>Crypto Wallet Redesign</p>
-                            </div>
-                            <div className="round">
-                                <input type="checkbox" name="task-check" id="task-check" />
-                                <label htmlFor="task-check"></label>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <span className="date">Today</span>
-                            <span className="time">10:00 PM - 11:45 PM</span>
-                        </div>
-                    </div>
-                    <div className="task-card">
-                        <div className="card-body">
-                            <div className="task">
-                                <h1>Client Review & Feedback</h1>
-                                <p>Crypto Wallet Redesign</p>
-                            </div>
-                            <div className="round">
-                                <input type="checkbox" name="task-check" id="task-check" />
-                                <label htmlFor="task-check"></label>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <span className="date">Today</span>
-                            <span className="time">10:00 PM - 11:45 PM</span>
-                        </div>
-                    </div>
+        <div className="center">
+            <div className="container">
+                {/* Your App Title */}
+                <h1 className="app-title">Your Tasks</h1>
+
+                {/* Todo List Task Input Form */}
+                <form action="#" method="post" id="form-todo">
+                    <input type="text" name="todo-task" id="todo-task" autoComplete="off" placeholder="Add new Task" />
+                    <button type="submit">
+                        <img src={plusIcon} className="btn-icon" alt="btn plus icon" />
+                    </button>
+                </form>
+
+                {/* Todo List Container */}
+                <div className="todo-list-container">
+                    <ul className="todo">
+                        <li className="todo-list-item">
+                            <input type="checkbox" name="todo-item-cbox" id="todo-item-cbox-1" />
+                            <span>React Learning</span>
+                            <button type="button" className="edit-item">
+                                <img src={penIcon} className="btn-icon" alt="btn pen icon" />
+                            </button>
+                            <button type="button" className="delete-item">
+                                <img src={trashIcon} alt="btn trash icon" />
+                            </button>
+                        </li>
+                        <li className="todo-list-item">
+                            <input type="checkbox" name="todo-item-cbox" id="todo-item-cbox-1" />
+                            <span>React Learning</span>
+                            <button type="button" className="edit-item">
+                                <img src={penIcon} className="btn-icon" alt="btn pen icon" />
+                            </button>
+                            <button type="button" className="delete-item">
+                                <img src={trashIcon} alt="btn trash icon" />
+                            </button>
+                        </li>
+                        <li className="todo-list-item">
+                            <input type="checkbox" name="todo-item-cbox" id="todo-item-cbox-1" />
+                            <span>React Learning</span>
+                            <button type="button" className="edit-item">
+                                <img src={penIcon} className="btn-icon" alt="btn pen icon" />
+                            </button>
+                            <button type="button" className="delete-item">
+                                <img src={trashIcon} alt="btn trash icon" />
+                            </button>
+                        </li>
+                        <li className="todo-list-item">
+                            <input type="checkbox" name="todo-item-cbox" id="todo-item-cbox-1" />
+                            <span>React Learning</span>
+                            <button type="button" className="edit-item">
+                                <img src={penIcon} className="btn-icon" alt="btn pen icon" />
+                            </button>
+                            <button type="button" className="delete-item">
+                                <img src={trashIcon} alt="btn trash icon" />
+                            </button>
+                        </li>
+                    </ul>
                 </div>
 
+                {/* Reminder of remaining task to finish */}
+                <div className="remaining-todo">
+                    <p>Your remaining todos: <span>0</span></p>
+                </div>
             </div>
         </div>
     )
